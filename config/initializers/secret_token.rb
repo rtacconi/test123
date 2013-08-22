19b020d36aa1fd270abfe759347edd0367179e47
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2013-08-22 10:25:04 +0100

    First post
    Silly app to test Jenkins

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test123::Application.config.secret_token = 'f9b42a9e0cb43577ab830ba9595a73b27279a3674e28488552b1eb41fd35baceb9e829d57fdc390087d7808fda999f2c2e8e075a3da15c22eb8e1e595497f247'
